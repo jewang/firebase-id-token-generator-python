66dc7a67cf0d625262681518a9c660e82f304ebd
oseeshogun <omasuaku@gmail.com>
2023-06-02 03:04:23 +0100

    ♻️ refactor: python 3 compatible

--- firebase_token_generator.py
+++ firebase_token_generator.py
@@ -1,18 +1,22 @@
 import argparse
 import json
-import urllib2
+from urllib import request, error as urllib_error
 
 import firebase_admin
 from firebase_admin import credentials, auth
 
+from dotenv import dotenv_values
+
+env_variables = dotenv_values('.env')
+
 # Configuration ================================================================
-DATABASE_URL = "https://[PROJECT_ID].firebaseio.com/"
+DATABASE_URL = f"https://{env_variables['PROJECT_ID']}.firebaseio.com/"
 # Download from
 # Firebase Console > Settings > Service Accounts > Generate New Private key
-PATH_TO_PRIVATE_KEY = "local/path/to/key.json"
+PATH_TO_PRIVATE_KEY = "./key.json"
 # Copy from
 # Firebase Console > Settings > General > Web API Key
-API_KEY = "[WEB_API_KEY]"
+API_KEY = env_variables["WEB_API_KEY"]
 # ==============================================================================
 
 cred = credentials.Certificate(PATH_TO_PRIVATE_KEY)
@@ -33,19 +37,22 @@ def get_token(uid):
   """
   token = auth.create_custom_token(uid)
   data = {
-    'token': token,
+    'token': token.decode('utf-8'),
     'returnSecureToken': True
   }
 
   url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty" \
         "/verifyCustomToken?key={}".format(API_KEY)
 
-  req = urllib2.Request(url,
-                        json.dumps(data),
-                        {'Content-Type': 'application/json'})
-  response = urllib2.urlopen(req).read()
-
-  return json.loads(response)
+  try:
+    data = json.dumps(data).encode()
+    req = request.Request(url, data=data, headers= {'Content-Type': 'application/json'})
+    response = request.urlopen(req).read()
+    return json.loads(response)
+  except urllib_error.HTTPError as e:
+    print(e.code)
+    print(e.read().decode("utf8", 'ignore'))
+    raise e
 
 
 if __name__ == "__main__":
@@ -54,4 +61,4 @@ if __name__ == "__main__":
   parser.add_argument("uid", help="Firebase User ID (UID)", type=str)
   args = parser.parse_args()
 
-  print get_token(args.uid)["idToken"]
+  print(get_token(args.uid)["idToken"])
